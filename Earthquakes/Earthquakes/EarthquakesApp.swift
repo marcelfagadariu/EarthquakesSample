ce80739093129f5c1eceec382140dbaf8c888441
Marcel Fagadariu <Marcel.Fagadariu@ikano.se>
2023-10-16 17:14:29 +0300

    first commit

---
+++ Earthquakes/Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Marcel on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
